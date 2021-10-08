e06fd853a372c7d04228f79e795ce24a94db3087
kannansk123 <92011051+kannansk123@users.noreply.github.com>
2021-10-08 22:15:23 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,32 @@
+const fastify = require('fastify');
+const PORT = process.env.PORT || 5000;
+const db = require("./config/db")
+const routes = require("./routes/postRoutes")
+const app = fastify({
+    logger: true
+})
+
+app.register(db)
+
+routes.forEach((route, index) => {
+
+    app.route(route)
+})
+app.get("/", async () => {
+    return {
+        Message: "Welcome"
+    }
+})
+
+const start = async () => {
+    try {
+        await app.listen(PORT)
+        app.log.info(`server listening on ${app.server.address().port}`)
+
+    } catch (err) {
+        app.log.error(err)
+        process.exit(1)
+
+    }
+}
+start();
